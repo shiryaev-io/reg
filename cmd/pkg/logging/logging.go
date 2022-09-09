c4926d29358220ccf39d46782c3e58011ee0f08f
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-09-09 15:55:13 +0300

    Добавляется пакет для логгирования

---
+++ cmd/pkg/logging/logging.go
@@ -0,0 +1,79 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+type writerHook struct {
+	Writer []io.Writer
+	LogLevel []logrus.Level
+}
+
+func (hook *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	for _, w := range hook.Writer {
+		w.Write([]byte(line))
+	}
+	return err
+}
+
+func (hook *writerHook) Levels() []logrus.Level {
+	return hook.LogLevel
+}
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() *Logger {
+	return &Logger{e}
+}
+
+func (l *Logger) GetLoggerWithField(k string, v interface{}) *Logger {
+	return &Logger{l.WithField(k, v)}
+}
+
+func init() {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			filename := path.Base(frame.File)
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	}
+
+	err := os.MkdirAll("logs", 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	allFile, err := os.OpenFile("logs/all.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
+	if err != nil {
+		panic(err)
+	}
+
+	l.SetOutput(io.Discard) // говорим logrus - ничего никуда не пиши! Т.к. нам нужно писать в 2 места сразу
+
+	l.AddHook(&writerHook{
+		Writer: []io.Writer{allFile, os.Stdout},
+		LogLevel: logrus.AllLevels,
+	})
+
+	l.SetLevel(logrus.TraceLevel)
+
+	e = logrus.NewEntry(l)
+}
