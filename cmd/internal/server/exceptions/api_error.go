0a0ad89fb31b05bfd7627f0ebbddf58afd9686a9
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-09-09 17:14:20 +0300

    Кастоманая ошибка

---
+++ cmd/internal/server/exceptions/api_error.go
@@ -0,0 +1,60 @@
+package exceptions
+
+import (
+	"encoding/json"
+	"net/http"
+	"reg/cmd/internal/res/strings"
+)
+
+// Кастомная ошибка запросов
+type ApiError struct {
+	Status     int
+	Err        error
+	Message    string
+	DevMessage string
+}
+
+// Реализация интрейфейса Error{}
+func (apiError *ApiError) Error() string {
+	return apiError.Message
+}
+
+// Возвращает массив байтов для передачи на клиент
+func (apiError *ApiError) Marshal() []byte {
+	marshal, err := json.Marshal(apiError)
+	if err != nil {
+		return nil
+	}
+	return marshal
+}
+
+// Возвращает ошибку, что пользователь не авторизован
+func UnauthorizedError(err error) *ApiError {
+	return &ApiError{
+		Status:     http.StatusUnauthorized,
+		Err:        err,
+		Message:    strings.ErrorUserUnauthorized,
+		DevMessage: err.Error(),
+	}
+}
+
+// Возвращает ошибку, если пользователь ввел неккоретные данные, 
+// не прошел валидацию и т.д.
+func BadRequest(message string, err error) *ApiError {
+	return &ApiError{
+		Status:     http.StatusBadRequest,
+		Err:        err,
+		Message:    message,
+		DevMessage: err.Error(),
+	}
+}
+
+// Ошибка сервера: не сгенерировался токен и т.д.
+func ServerError(message string, err error) *ApiError {
+	return &ApiError{
+		Status:     http.StatusInternalServerError,
+		Err:        err,
+		Message:    message,
+		DevMessage: err.Error(),
+	}
+}
