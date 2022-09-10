b89afafec59daa21eef16d64863daf6a11b43a28
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-09-10 20:02:36 +0300

    Обработка сигналов и запуск сервера

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,115 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+	"reg/cmd/internal/server"
+	"reg/cmd/internal/server/adapters/db/postgresql"
+	"reg/cmd/internal/server/config"
+	"reg/cmd/internal/server/routes"
+	"reg/cmd/internal/server/services"
+	"reg/cmd/pkg/logging"
+	"syscall"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	// TODO: implement
+	logger := logging.GetLogger()
+	
+	// Канал для сигналов
+	sig := make(chan bool)
+	// Основной канал
+	loop := make(chan error)
+
+	// Мониторинг сигналов
+	go listenerSignal(sig, logger)
+
+	for quit := false; !quit; {
+		go func() {
+			initAndRunServer(logger)
+			loop <- nil
+		}()
+
+		// Блокировка программы при получении сигнала
+		select {
+		// Прерывается выполнение программы
+		case quit = <-sig:
+		// Продолжается выполлнение программы
+		case <-loop:
+		}
+	}
+}
+
+func listenerSignal(sig chan bool, logger *logging.Logger) {
+	var quit bool
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(
+		c,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGHUP,
+	)
+
+	for signal := range c {
+		// logger.Infof(strings.LogGetSignalSuccess, signal.String())
+
+		switch signal {
+		case syscall.SIGINT, syscall.SIGTERM:
+			quit = true
+		case syscall.SIGHUP:
+			quit = false
+		}
+
+		if quit {
+			quit = false
+			// TODO: closeDB(), closeLog()
+		}
+
+		// Оповещение о прекращении работы
+		sig <- quit
+	}
+}
+
+func initAndRunServer(logger *logging.Logger) {
+	// Получение конфигурации
+	dbConfig := config.NewConfigDb()
+	// Коннект к БД
+	registrationDatabase, err := postgresql.NewRegistrationDatabase(
+		context.Background(),
+		dbConfig,
+		logger,
+	)
+	if err != nil {
+		// Logging
+	}
+
+	userService := &services.UserService{
+		UserStorage: registrationDatabase,
+		Logger:      logger,
+	}
+	regService := &services.RegistrationService{
+		UserService: userService,
+		Logger:      logger,
+	}
+
+	router := mux.NewRouter()
+
+	apiRouter := &routes.ApiRoute{
+		Router:     router,
+		RegService: regService,
+		Logger:     logger,
+	}
+
+	// Инициализация сервера
+	serv := server.NewServer(
+		apiRouter,
+		regService,
+		logger,
+	)
+
+	// Запуск сервера
+	serv.Run()
 }
