9c9f4d8dd9ed5476a7bb81b917be7fa0ec648003
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-09-10 19:58:35 +0300

    Создание и инициализация сервера

--- cmd/internal/server/server.go
+++ cmd/internal/server/server.go
@@ -1,1 +1,45 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"reg/cmd/internal/server/routes"
+	"reg/cmd/internal/server/services"
+	"reg/cmd/pkg/logging"
+)
+
+const (
+	serviceHost = "SERVICE_HOST"
+	servicePort = "SERVICE_PORT"
+)
+
+// Структура сервера
+type server struct {
+	router     *routes.ApiRoute
+	regService *services.RegistrationService
+	logger     *logging.Logger
+}
+
+// Инициалиазция сервера
+func NewServer(
+	router *routes.ApiRoute,
+	regService *services.RegistrationService,
+	logger     *logging.Logger,
+) *server {
+	router.Init()
+	return &server{
+		router:     router,
+		regService: regService,
+		logger:     logger,
+	}
+}
+
+// Запускает сервер
+func (server *server) Run() {
+	host := os.Getenv(serviceHost)
+	port := os.Getenv(servicePort)
+	serviceUrl := host + ":" + port
+
+	log.Fatal(http.ListenAndServe(serviceUrl, server.router.Router))
+}
