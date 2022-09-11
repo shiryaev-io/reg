5742b4ea3c1fc7c0bff5d8f3bb3fa03783cca85d
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-09-11 10:34:27 +0300

    Создаются строки для логов

--- cmd/internal/res/strings/strings_log.go
+++ cmd/internal/res/strings/strings_log.go
@@ -1,5 +1,26 @@
 package strings
 
 const (
-	
+	LogGetSignalSuccess  = "Сигнал получен!"
+	LogGetDatabaseError  = "База данных не получена"
+	LogGetHostAndPortEnv = "Получение Host и Port из файла переменных окружения .env"
+	LogRunServer         = "Запуск сервера на URL: %s"
+
+	LogAttemptConnectDb = "Попытка подключения к БД: %s"
+	LogTryConnectDb     = "Подключение к БД"
+	LogFatalConnectDb   = "Не удалось подключиться к БД, ошибка: %v"
+	LogConnectSuccess   = "База данных успешно подключена"
+
+	LogGettingRequestBody            = "Получение тела запроса Body"
+	LogErrorInvalidRequestBodyFormat = "Неверный формат тела запроса: %v"
+	LogRegistration                  = "Регистрация пользователя"
+	LogErrorRegistration             = "Не удалось зарегистрироваться: %v"
+
+	LogFingUserByEmail             = "Поиск пользователя по email"
+	LogErrorFindUser               = "Ошибка поиска пользователя в БД: %v"
+	LogErrorUserAlreadyExists      = "Пользователь уже существует в БД"
+	LogGenerateHashedPassword      = "Генерация захешированного пароля"
+	LogErrorGenerateHashedPassword = "Ошибка генерации захешированного пароля: %v"
+	LogCreateUserInDb              = "Создание пользователя в БД"
+	LogErrorCreateUserInDb         = "Ошибка создания пользователя в БД: %v"
 )
