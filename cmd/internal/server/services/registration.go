ee7bb35fbfa8e752bf7825eeb4e2f7e07152322c
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-09-11 13:12:33 +0300

    Валидация данных пользователей

--- cmd/internal/server/services/registration.go
+++ cmd/internal/server/services/registration.go
@@ -1,10 +1,30 @@
 package services
 
-import "reg/cmd/pkg/logging"
+import (
+	"reg/cmd/internal/res/strings"
+	"reg/cmd/pkg/logging"
+)
 
 // Сервис регистрации
 // Содержит другие сервисы
 type RegistrationService struct {
-	UserService *UserService
-	Logger      *logging.Logger
+	UserService       *UserService
+	ValidationService *ValidationService
+	Logger            *logging.Logger
+}
+
+// Регистрация пользователя
+func (service *RegistrationService) Registration(email, password string) error {
+	service.Logger.Infoln(strings.LogStartValidData)
+
+	err := service.ValidationService.Validate(email, password)
+	if err != nil {
+		service.Logger.Errorf(strings.LogErrorValidData, err)
+
+		return err
+	}
+
+	service.Logger.Infoln(strings.LogStartRegistration)
+
+	return service.UserService.Registration(email, password)
 }
