35d117d92b64dde9238fcfedb1fa21f4af47ab4a
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-09-11 10:49:22 +0300

    Логирования сервисов

--- cmd/internal/server/services/user.go
+++ cmd/internal/server/services/user.go
@@ -2,9 +2,11 @@ package services
 
 import (
 	"errors"
+	"reg/cmd/internal/res/strings"
 	"reg/cmd/internal/server/models/db"
 	"reg/cmd/pkg/logging"
 
+	"github.com/jackc/pgx/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -24,27 +26,41 @@ func (service *UserService) Registration(
 	email, password string,
 ) error {
 	// TODO: валидация пароля
+	service.Logger.Infoln(strings.LogFingUserByEmail)
 	// Проверка, что пользователь уже существует
 	user, err := service.FindByEmail(email)
-	if err != nil {
+	if err != nil && err != pgx.ErrNoRows {
+		service.Logger.Errorf(strings.LogErrorFindUser, err)
+
 		return err
 	}
 	if user != nil {
+		service.Logger.Errorln(strings.LogErrorUserAlreadyExists)
+
 		return errors.New("Пользователь с почтовым адресом " + email + " уже существует")
 	}
+
+	service.Logger.Infoln(strings.LogGenerateHashedPassword)
 
 	hashPassword, err := bcrypt.GenerateFromPassword(
 		[]byte(password),
 		bcrypt.DefaultCost,
 	)
 	if err != nil {
+		service.Logger.Errorln(strings.LogErrorGenerateHashedPassword)
+
 		return err
 	}
+
+	service.Logger.Infoln(strings.LogCreateUserInDb)
+
 	err = service.UserStorage.Create(
 		email,
 		string(hashPassword),
 	)
 	if err != nil {
+		service.Logger.Errorf(strings.LogErrorCreateUserInDb, err)
+
 		return err
 	}
 
